8e1bc8671957c8b9f9d144fcbac23aff753dbec3
Wilfriedkouame <wilfriedaimeyao@gmail.com>
2021-02-24 02:05:37 +0000

    mon premier commit

---
+++ appFile/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AppfileConfig(AppConfig):
+    name = 'appFile'
---
+++ appFile/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import Index
+
+
+
+
+
+
+urlpatterns = [
+    path('', Index),
+   
+]
